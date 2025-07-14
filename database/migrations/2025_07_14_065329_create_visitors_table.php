2773df7468ff1f0ba9b278d7b1f461135b327375
osmanndnc <osmannuri.dinc21@ogr.atauni.edu.tr>
2025-07-14 10:38:01 +0300

    Allah yar ve yardımcımız olsun

---
+++ database/migrations/2025_07_14_065329_create_visitors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+             $table->string('name');
+            $table->string('tc_no')->unique();
+            $table->string('phone');
+            $table->string('plate')->nullable();
+            $table->unsignedBigInteger('approved_by');
+            $table->timestamps();
+
+              $table->foreign('approved_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
