347a6a1ed7954df7eb3b3c72c0f8ef649e49a6f5
osmanndnc <osmannuri.dinc21@ogr.atauni.edu.tr>
2025-07-15 14:12:20 +0300

    Login aktif edildi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                  <img src="{{ asset('images/ata_icon.png') }}" alt="Ata Logo" style="width: 200px; height: 60px;" class="mx-auto my-4" />
                     </a>
                 </div>
 
